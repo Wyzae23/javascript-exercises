e1721d433c3e728f38058d138a32d770d85a239e
Harold <haroldmo23@gmail.com>
2022-06-29 21:24:37 -0700

    do exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) 
+{
+    if(!Number.isInteger(num1) || !Number.isInteger(num2) || num1 < 0 || num2 < 0)
+    {
+        return "ERROR";
+    }
+    if(num1 > num2)
+    {
+        let tmp = num1;
+        num1 = num2;
+        num2 = tmp;
+    }
+    let ans = 0;
+    while(num1 <= num2)
+    {
+        ans += num1;
+        num1++;
+    }
 
+    return ans;
 };
 
 // Do not edit below this line
