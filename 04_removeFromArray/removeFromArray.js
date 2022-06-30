2e5c4dd3500c25f80713ece617a01f72a9ed91ef
Harold <haroldmo23@gmail.com>
2022-06-29 20:55:08 -0700

    do exercises 1-4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function() 
+{
+    console.log(arguments);
+    let arr = arguments[0];
+    for(let i = 1; i < arguments.length; i++)
+    {
+        let idx = arr.indexOf(arguments[i]);
+        if(idx >= 0 && idx < arr.length)
+        {
+            arr.splice(idx, 1);
+        }
+        
+    }
 
-};
+    return arr;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
